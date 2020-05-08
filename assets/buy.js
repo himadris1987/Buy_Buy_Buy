9b8126007990ed9261afcb73db00a6a89c5b6947
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-05-07 19:09:48 -0700

    added the $.ajax for euroUSDBtn

---
+++ assets/buy.js
@@ -0,0 +1,25 @@
+// function to access euro/usd api
+$("#euroUSDBtn").on("click", function() {
+
+    // currency to convert variables
+    var currency1 = "EUR"
+    var currency2 = "USD"
+    // convert from currency1 to currency2
+    var queryURL = "https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=" + currency1 + "&to=" + currency2 ,
+    
+    // passed to $.ajax to recieve response
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": queryURL,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+            "x-rapidapi-key": "2baeb4eca6mshb648eeb18d253bdp1392e1jsnf42824529a45"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+  });
